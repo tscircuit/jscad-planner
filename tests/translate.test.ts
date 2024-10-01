326ebaef88d024dd2d4eb5e206182e78d3abef32
seveibar <seveibar@gmail.com>
2024-10-01 13:54:02 -0700

    add translate test

---
+++ tests/translate.test.ts
@@ -0,0 +1,15 @@
+import { expect, it } from "bun:test"
+import { jscadPlanner } from "../lib/jscad-planner"
+
+it("should be able to translate a shape", () => {
+  const shape = jscadPlanner.primitives.cube({ size: 10, center: [10, 0, 0] })
+  const translatedShape = jscadPlanner.transformations.translate(
+    [10, 0, 0],
+    shape,
+  )
+  expect(translatedShape).toEqual({
+    type: "translate",
+    vector: [10, 0, 0],
+    shape,
+  })
+})
