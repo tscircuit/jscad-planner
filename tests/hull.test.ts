7ed25c35254e8dbbb8db19454fe08bd3abcf3109
Anas sarkiz <anassoftwaredev@gmail.com>
2024-11-24 18:14:38 +0200

    added test

--- tests/hull.test.ts
+++ tests/hull.test.ts
@@ -4,9 +4,10 @@ import { jscadPlanner } from "../lib/jscad-planner"
 it("should be able to hull shapes", () => {
   const shape1 = jscadPlanner.primitives.cube({ size: 2, center: [10, 0, 0] })
   const shape2 = jscadPlanner.primitives.cube({ size: 10, center: [0, 0, 0] })
-  const translatedShape = jscadPlanner.booleans.hull( shape1, shape2)
-  expect(translatedShape).toEqual({
+  const hullShape = jscadPlanner.booleans.hull( shape1, shape2)
+  expect(hullShape).toEqual({
     type: "hull",
-    ["shapes"]: [shape1, shape2],
+    shapes: [shape1, shape2],
   })
 })
+ 
